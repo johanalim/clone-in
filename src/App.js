e8c3246e313a77af73024e6b22dd1a312bd39eac
JohanIT <alimjohan@gmail.com>
2023-11-08 19:12:05 +0700

    update stuckture folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 
-function App() {
+const Home = React.lazy(() => import("./container/pages/Home"));
+const About = React.lazy(() => import("./container/pages/About"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </Router>
+  )
 }
-
 export default App;
